f20778728fa0dbc42f273a74c3687a1525487c61
Tatyana Yanovich <belisenta@gmail.com>
2022-10-10 11:29:48 +0300

    updated aftter feedback

--- HomeWork_12.8/Program.cs
+++ HomeWork_12.8/Program.cs
@@ -1,4 +1,5 @@
 ﻿using static System.Console;
+using static System.DateTime;
 using static System.Int32;
 
 namespace HomeWork_12._8;
@@ -45,15 +46,36 @@ internal class Program
 
             case 2:
                 Clear();
-                repository.CreateNewEntryFromUserInput();
+
+                int id = repository.existingWorkers.Max(worker => worker.Id) + 1;
+                WriteLine("\nPlease, enter full name: ");
+                string fio = ReadLine() ?? string.Empty;
+                WriteLine("\nPlease, enter age: ");
+                TryParse(ReadLine(), out int age);
+                WriteLine("\nPlease, enter height: ");
+                TryParse(ReadLine(), out int height);
+                WriteLine("\nPlease, enter date of birth (YYYY.MM.DD): ");
+                TryParse(ReadLine(), out DateTime dateOfBirth);
+                WriteLine("\nPlease, enter place of birth: ");
+                string placeOfBirth = ReadLine() ?? string.Empty;
+                WriteLine("\nPlease, enter date of entry creation (YYYY.MM.DD): ");
+                TryParse(ReadLine(), out DateTime dateOfEntryCreation);
+                Clear();
+
+                Worker newWorker = new(id, fio, age, height, dateOfBirth, placeOfBirth, dateOfEntryCreation);
+                repository.AddWorker(newWorker);
                 repository.UpdateDatabase();
+
                 WriteLine("Entry added!");
                 ReturnToMenu(repository);
                 break;
 
             case 3:
                 Clear();
-                repository.DeleteEntryByUserInput();
+                WriteLine("Please, enter ID of entry you wish to delete: ");
+                TryParse(ReadLine(), out int idToDelete);
+                Clear();
+                repository.DeleteWorker(idToDelete); 
                 repository.UpdateDatabase();
                 WriteLine("Entry deleted!");
                 ReturnToMenu(repository);
@@ -61,13 +83,27 @@ internal class Program
 
             case 4:
                 Clear();
-                repository.ChooseSortingOfEntriesByParameter();
+                ChooseSortingOfEntriesByParameter(repository);
                 ReturnToMenu(repository);
                 break;
 
             case 5:
                 Clear();
-                repository.PrintEntriesFilteredByUserInsertedDateRange();
+
+                WriteLine("Please, enter first date (YYYY.MM.DD): ");
+                TryParse(ReadLine(), out DateTime from);
+                WriteLine("Please, enter second date (YYYY.MM.DD): ");
+                TryParse(ReadLine(), out DateTime to);
+                Clear();
+
+                List<Worker> filteredWorkers = repository.GetWorkersBetweenTwoDates(from, to);
+
+                if (filteredWorkers.Any())
+                    foreach (Worker worker in filteredWorkers)
+                        WriteLine(worker.Print());
+                else
+                    WriteLine("No entries available"); 
+                
                 ReturnToMenu(repository);
                 break;
 
@@ -75,6 +111,77 @@ internal class Program
                 WriteLine("\nUncknown command");
                 ReturnToMenu(repository);
                 break;
+        }
+    }
+
+    private static void ChooseSortingOfEntriesByParameter(Repository repository)
+    {
+        WriteLine("Choose sorting parameter:\n\n" +
+                  "(1) ID\n" +
+                  "(2) FullName\n" +
+                  "(3) Age\n" +
+                  "(4) Height\n" +
+                  "(5) Date of birth\n" +
+                  "(6) Place of birth\n" +
+                  "(7) Date of entry creation\n" +
+                  "\nEnter (1), (2), (3), (4), (5), (6), (7)");
+
+        TryParse(ReadLine(), out int choice);
+        IEnumerable<Worker> orderedEnumerable;
+
+        switch (choice)
+        {
+            case 1:
+                Clear();
+                orderedEnumerable = repository.existingWorkers.OrderBy(worker => worker.Id);
+                PrintSortedEntries(orderedEnumerable);
+                break;
+
+            case 2:
+                Clear();
+                orderedEnumerable = repository.existingWorkers.OrderBy(worker => worker.Fio);
+                PrintSortedEntries(orderedEnumerable);
+                break;
+
+            case 3:
+                Clear();
+                orderedEnumerable = repository.existingWorkers.OrderBy(worker => worker.Age);
+                PrintSortedEntries(orderedEnumerable);
+                break;
+
+            case 4:
+                Clear();
+                orderedEnumerable = repository.existingWorkers.OrderBy(worker => worker.Height);
+                PrintSortedEntries(orderedEnumerable);
+                break;
+
+            case 5:
+                Clear();
+                orderedEnumerable = repository.existingWorkers.OrderBy(worker => worker.DateOfBirth);
+                PrintSortedEntries(orderedEnumerable);
+                break;
+
+            case 6:
+                Clear();
+                orderedEnumerable = repository.existingWorkers.OrderBy(worker => worker.PlaceOfBirth);
+                PrintSortedEntries(orderedEnumerable);
+                break;
+
+            case 7:
+                Clear();
+                orderedEnumerable = repository.existingWorkers.OrderBy(worker => worker.DateOfEntryCreation);
+                PrintSortedEntries(orderedEnumerable);
+                break;
+
+            default:
+                WriteLine("Uncknown command");
+                break;
+        }
+
+        static void PrintSortedEntries(IEnumerable<Worker> orderedEnumerable)
+        {
+            foreach (Worker worker in orderedEnumerable)
+                WriteLine(worker.Print());
         }
     }
 }
--- HomeWork_12.8/Repository.cs
+++ HomeWork_12.8/Repository.cs
@@ -7,7 +7,7 @@ namespace HomeWork_12._8;
 internal class Repository 
 {
     private string pathFile;
-    private List<Worker> existingWorkers;
+    internal List<Worker> existingWorkers;
     
 
     public Repository(string pathFile)
@@ -46,15 +46,6 @@ internal class Repository
         existingWorkers.ForEach(worker => sw.WriteLine(worker.NewEntry()));
     }
 
-    //Метод удаляет запись в файле по введенному пользователем ID
-    public void DeleteEntryByUserInput()
-    {
-        WriteLine("Please, enter ID of entry you wish to delete: ");
-        TryParse(ReadLine(), out int idToDelete);
-        Clear();
-        DeleteWorker(idToDelete);
-    }
-
     //Метод записывает новый Worker в файл
     public void AddWorker(Worker worker)
     {
@@ -62,101 +53,6 @@ internal class Repository
         sw.WriteLine(worker.NewEntry());
     }
 
-    //Метод создает новую запись в файле из введенных пользователем параметров
-    public void CreateNewEntryFromUserInput()
-    {
-        int id = existingWorkers.Max(worker => worker.Id) + 1;
-
-        WriteLine("\nPlease, enter full name: ");
-        string fio = ReadLine() ?? string.Empty;
-
-        WriteLine("\nPlease, enter age: ");
-        TryParse(ReadLine(), out int age);
-
-        WriteLine("\nPlease, enter height: ");
-        TryParse(ReadLine(), out int height);
-
-        WriteLine("\nPlease, enter date of birth (YYYY.MM.DD): ");
-        TryParse(ReadLine(), out DateTime dateOfBirth);
-
-        WriteLine("\nPlease, enter place of birth: ");
-        string placeOfBirth = ReadLine() ?? string.Empty;
-
-        WriteLine("\nPlease, enter date of entry creation (YYYY.MM.DD): ");
-        TryParse(ReadLine(), out DateTime dateOfEntryCreation);
-
-        Clear();
-
-        Worker newWorker = new(id, fio, age, height, dateOfBirth, placeOfBirth, dateOfEntryCreation);
-        AddWorker(newWorker);
-    }
-
-    //Метод позволяет пользователю выбрать параметр для сортировки и вывода записей
-    public void ChooseSortingOfEntriesByParameter()
-    {
-        WriteLine("Choose sorting parameter:\n\n" +
-                  "(1) ID\n" +
-                  "(2) FullName\n" +
-                  "(3) Age\n" +
-                  "(4) Height\n" +
-                  "(5) Date of birth\n" +
-                  "(6) Place of birth\n" +
-                  "(7) Date of entry creation\n" +
-                  "\nEnter (1), (2), (3), (4), (5), (6), (7)");
-
-        TryParse(ReadLine(), out int choice);
-        IEnumerable<Worker> orderedEnumerable;
-
-        switch (choice)
-        {
-            case 1:
-                Clear();
-                orderedEnumerable = existingWorkers.OrderBy(worker => worker.Id);
-                PrintSortedEntries(orderedEnumerable);
-                break;
-
-            case 2:
-                Clear();
-                orderedEnumerable = existingWorkers.OrderBy(worker => worker.Fio);
-                PrintSortedEntries(orderedEnumerable);
-                break;
-
-            case 3:
-                Clear();
-                orderedEnumerable = existingWorkers.OrderBy(worker => worker.Age);
-                PrintSortedEntries(orderedEnumerable);
-                break;
-
-            case 4:
-                Clear();
-                orderedEnumerable = existingWorkers.OrderBy(worker => worker.Height);
-                PrintSortedEntries(orderedEnumerable);
-                break;
-
-            case 5:
-                Clear();
-                orderedEnumerable = existingWorkers.OrderBy(worker => worker.DateOfBirth);
-                PrintSortedEntries(orderedEnumerable);
-                break;
-
-            case 6:
-                Clear();
-                orderedEnumerable = existingWorkers.OrderBy(worker => worker.PlaceOfBirth);
-                PrintSortedEntries(orderedEnumerable);
-                break;
-
-            case 7:
-                Clear();
-                orderedEnumerable = existingWorkers.OrderBy(worker => worker.DateOfEntryCreation);
-                PrintSortedEntries(orderedEnumerable);
-                break;
-
-            default:
-                WriteLine("Uncknown command");
-                break;
-        }
-    }
-    
     // Метод считывает данные из файла, фильтрует записи по запрошенному диапазону, возращает массив соответствующих значений
     public List<Worker> GetWorkersBetweenTwoDates(DateTime dateFrom, DateTime dateTo)
     {
@@ -171,27 +67,7 @@ internal class Repository
 
         return filteredWorkers;
     }
-
-    // Метод считывает диапазон дат введенных пользователем и выводит в консоль отфильтрованные по данному диапазону записи
-    public void PrintEntriesFilteredByUserInsertedDateRange()
-    {
-        WriteLine("Please, enter first date (YYYY.MM.DD): ");
-        TryParse(ReadLine(), out DateTime from);
-
-        WriteLine("Please, enter second date (YYYY.MM.DD): ");
-        TryParse(ReadLine(), out DateTime to);
-
-        Clear();
-
-        List<Worker> filteredWorkers = GetWorkersBetweenTwoDates(from, to);
-
-        if (filteredWorkers.Any())
-            foreach (Worker worker in filteredWorkers)
-                WriteLine(worker.Print());
-        else
-            WriteLine("No entries available");
-    }
-
+    
 
     // Метод считывает данные из файла и возвращает массив из Worker
     private List<Worker> GetAllWorkers()
@@ -224,12 +100,5 @@ internal class Repository
         using StreamWriter sw = new(pathFile, true);
         Worker worker = new();
         sw.WriteLine(worker.NewEntry());
-    }
-
-    //Метод выводит в консоль записи отсортированные по параметру 
-    private static void PrintSortedEntries(IEnumerable<Worker> orderedEnumerable)
-    {
-        foreach (Worker worker in orderedEnumerable)
-            WriteLine(worker.Print());
     }
 }
